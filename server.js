e0f2541fba95c1088e4ad4052b552ab0d8bf59ab
julianbrodsky <brodskyjulian@yahoo.com>
2022-08-12 00:26:38 -0600

    setting up CRUD

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const PORT = 2022
 
@@ -6,6 +7,12 @@ app.listen(2022, function(){
     console.log(`Your fridge is running on port ${PORT}, better catch it!`)
 })
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/stocks', function(req, res){
+console.log(req.body)
 })
