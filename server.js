8287ea6e41aa0ba0ca98b5f48d31b5396dbbfff9
julianbrodsky <brodskyjulian@yahoo.com>
2022-08-12 00:55:24 -0600

    added env

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const dotenv = require('dotenv')
+dotenv.config()
+const MongoClient = require('mongodb').MongoClient
 const app = express()
 const PORT = 2022
+const url = process.env.MONGOURL
+
+MongoClient.connect(url, { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to Database')
+  })
+  .catch(error => console.error(error))
 
 app.listen(2022, function(){
     console.log(`Your fridge is running on port ${PORT}, better catch it!`)
